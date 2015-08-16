a5a887a970779d01505cb7a03ca9de824b41ba71
taro <taroimotaro2727@gmail.com>
2015-08-16 14:02:28 +0900

    api

--- conf.php
+++ conf.php
@@ -7,13 +7,9 @@ define('_TWITTER_OAUTH_PATH', _SYSTEM_ROOT.'twitteroauth/');
 //twitterapi
 define('_TWITTER_API_PATH', _SYSTEM_ROOT.'twitterapi/');
 
-//TwitterAPI
-define('CONSUMER_KEY', 'kWasFaoJzD2X6a5b3Zm8U4K2x');
-define('CONSUMER_SECRET', '8w6Q0lKeJ2pJgZukYe9biKO46WIJBeE6UvAk5pweDvgGexGSDW');
-define('ACCESS_TOKEN', '3252741534-ZEUvpj0JbtFxo9w7i1wZVwzCEuwQIweWrGImXQt');
-define('ACCESS_TOKEN_SECRET', 'OJQFcixC8siZSj3OR7xUHz6zwedT8nZOLe4v9xUE7KrkB');
 
 //require
 //require_once(_TWITTER_OAUTH_PATH."src/TwitterOAuth.php");
 // OAuthÉâÉCÉuÉâÉäÇÃì«Ç›çûÇ›
 require_once _TWITTER_OAUTH_PATH.'autoload.php';
+require_once _TWITTER_API_PATH.'api_setting.php';
