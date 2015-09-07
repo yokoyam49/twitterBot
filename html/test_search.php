c484d7415469468dbd0f42f0ddbcbea6524dec1f
yokoyama <taro_yokoyama@precs.co.jp>
2015-09-07 17:40:04 +0900

    search

---
+++ html/test_search.php
@@ -0,0 +1,12 @@
+<html lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>サーチテスト</title>
+</head>
+<body>
+<form name="search_form" id="search_form" method="post" action="./test.php">
+<input type="hidden" name="id" value="1">
+<input type="text" name="search" value="">
+</form>
+</body>
+</html>
