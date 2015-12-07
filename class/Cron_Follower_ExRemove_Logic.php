3251d9f28c3d843ef9122db002c5578c47be0c02
yokoyama <taro_yokoyama@precs.co.jp>
2015-12-07 11:10:57 +0900

    unix

--- class/Cron_Follower_ExRemove_Logic.php
+++ class/Cron_Follower_ExRemove_Logic.php
@@ -171,7 +171,8 @@ class Cron_Follower_ExRemove_Logic
         $sql = "DELETE FROM rss_clickcount_hosts WHERE create_date <= ?";
         $delete_count = $this->DBobj->execute($sql, array($delete_since_date));
 
-        $mes = date("Y-m-d H:i:s")." rss_clickcount_hostsテーブル ".$delete_count ? $delete_count : '0'."件 データ削除処理実行\n";
+        $delete_count = $delete_count ? $delete_count : 0;
+        $mes = date("Y-m-d H:i:s")." rss_clickcount_hostsテーブル ".(string)$delete_count."件 データ削除処理実行\n";
         error_log($mes, 3, _TWITTER_LOG_PATH.$this->logFile);
     }
 
